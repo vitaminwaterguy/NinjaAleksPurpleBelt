c561540bf9c9f3950c1c8efa7f7246f8257edaba
MasterHacker10 <miranda.c.negro@gmail.com>
2025-01-31 15:56:18 -0500

    idk

--- Meany Bird/Assets/Scripts/PlayerControls.cs
+++ Meany Bird/Assets/Scripts/PlayerControls.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour {[Header("Game Controller Object for controlling the game")]
+public class PlayerControls : MonoBehaviour 
+{
+    [Header("Game Controller Object for controlling the game")]
     public GameController gameController;
     [Header("Default Velocity")]
     public float velocity = 5;
@@ -13,7 +15,7 @@ public class PlayerControls : MonoBehaviour {[Header("Game Controller Object for
     {
         gameController = GetComponent<GameController>();
         Time.timeScale = 1;
-        rb = GetComponent<Rigidbody2D > ();
+        rb = GetComponent<Rigidbody2D> ();
         objectHeight =
                 transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
