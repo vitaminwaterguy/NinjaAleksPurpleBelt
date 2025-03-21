2f71e5c1636f9b57dee129c0f1ead481892839fc
MasterHacker10 <aleks.strzelecki@icloud.com>
2025-03-21 15:59:21 -0400

    got a lot of work done
    t

---
+++ New Unity Project/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Jase is so cool and so is brooks")]
+    public Rigidbody2D rb;
+    public float downSpeed = 20f;
+    public float movementSpeed = 10f;
+    public float movenment = 0f;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
